2d48ff22a33c9a9a42d5fe2af2a9d830df1d5794
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-01-27 13:46:36 +0200

    changes in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,42 @@
 import Button from "./components/Button";
 import Form from "./components/Form";
 import Input from "./components/Input";
+import axios from "axios";
+import { useState } from "react";
+import RepoInfo from "./components/RepoInfo";
 
 const App = () => {
+  const [ username, setUsername ] = useState(null);
+  const [ data, setData ] = useState({});
+  const [ error, setError ] = useState(false);
+
+  const sendUsername = async () => {
+    try {
+      error && setError(null);
+      const response = await axios.get(`http://localhost:9876/repos/${ username }`)
+      setData(response.data);
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  }
+
   return (
     <Form>
         <h3>Search repositories on github by username</h3>
         <Input
-           initialValue="" 
-           placeholder="Input username"
+           initialValue = "" 
+           placeholder = "Input username"
+           callback = { setUsername }
         />
 
       <Button
-        onClick = { () => console.log("click") }
-        isDisable = { false }
+        onClick = { sendUsername }
+        isDisable = { !username }
       >
         Find
       </Button>
+      { error && (<div>{ error }</div>) }
+      <RepoInfo data={ data } />
     </Form>
   );
 }
